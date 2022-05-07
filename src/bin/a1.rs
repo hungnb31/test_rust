3c7598654dba92b0bf55cf5e80597309694ab160
Hung Nguyen Ba <hungnb31@gmail.com>
2022-05-07 10:17:21 +0700

    prac: first update

---
+++ src/bin/a1.rs
@@ -0,0 +1,12 @@
+fn first_name() {
+  println!("Howard");
+}
+
+fn last_name() {
+  println!("Carter");
+}
+
+fn main() {
+  first_name();
+  last_name();
+}
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+// one line comment
+
+/*
+multiple 
+line
+comment
+*/
+fn main() {
+    println!("Hello, world!");
+}
+
